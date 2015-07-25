4c2e5602fda2c9fdf1dbb807d20f711e96e42e9a
Karim <kcheurfi@outlook.com>
2015-07-26 01:17:59 +0200

    Menu selection joueur et refactor de la base du menu

---
+++ Projet/i3gamejam2015/Assets/Scripts/Interface/PlayersSelectorManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayersSelectorManager : MonoBehaviour {
+
+    private PlayerSelectorManager[] listPlayerSelector;
+    public int minPlayerNeeded = 3;
+
+	// Use this for initialization
+	void Start () {
+
+        listPlayerSelector = GetComponentsInChildren<PlayerSelectorManager>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        int nbReady = 0;
+        bool canStart = true;
+        for (int i = 0; i < listPlayerSelector.Length; i++)
+        {
+            if (listPlayerSelector[i].HasPressedStart)
+            {
+                if (listPlayerSelector[i].HasChoosen)
+                {
+                    nbReady++;
+                }
+                else
+                {
+                    canStart = false;
+                }
+            }
+
+        }
+
+        if (canStart && (nbReady >= minPlayerNeeded))
+        {
+            Application.LoadLevelAdditiveAsync("SelectLvl");
+            Destroy(gameObject);
+        }
+	
+	}
+}

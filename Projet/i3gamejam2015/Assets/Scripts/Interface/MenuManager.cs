62324805cdbb278303add5c9eb612aa22cded633
Karim <kcheurfi@outlook.com>
2015-07-25 18:56:31 +0200

    Ajout du Menu

---
+++ Projet/i3gamejam2015/Assets/Scripts/Interface/MenuManager.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour {
+
+    enum StartMenuItem { GameStart, HowTo, Quit };
+    private string dpadHorizontal = "Horizontal";
+    private string dpadVertical = "Vertical";
+    private Color32 highlithed = new Color32(107, 107, 107, 255);
+    private Color32 normal = new Color32(0, 0, 0, 255);
+    
+    public GameObject StartButtonArea;
+    private Outline[] buttonList;
+
+    private StartMenuItem menuSelectedItem = StartMenuItem.GameStart;
+
+    private bool[] wasPressed = new bool[4];
+
+
+	// Use this for initialization
+	void Start () {
+        buttonList = StartButtonArea.GetComponentsInChildren<Outline>();
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 1; i < 5; i++ )
+            CheckControlerStartMenu(i);
+	}
+
+    void CheckControlerStartMenu(int noControler)
+    {
+
+        if (Input.GetButtonDown(InputManager.A + " P" + noControler))
+        {
+            switch (menuSelectedItem){
+            
+                case StartMenuItem.Quit : Application.Quit();
+                    break;
+
+                case StartMenuItem.GameStart: Application.LoadLevel(1);
+                    break;
+            }
+        }
+
+        if (!wasPressed[noControler-1] && Input.GetAxis(dpadVertical + noControler) < 0)
+        {
+            if (menuSelectedItem != (StartMenuItem)0)
+            {
+                buttonList[(int)menuSelectedItem].effectColor = normal;
+                menuSelectedItem -= 1;
+                buttonList[(int)menuSelectedItem].effectColor = highlithed;
+                wasPressed[noControler - 1] = true;
+            }
+            
+        }
+        else if (!wasPressed[noControler - 1] && Input.GetAxis(dpadVertical + noControler) > 0)
+        {
+            if (menuSelectedItem != StartMenuItem.Quit)
+            {
+                buttonList[(int)menuSelectedItem].effectColor = normal;
+                menuSelectedItem += 1;
+                buttonList[(int)menuSelectedItem].effectColor = highlithed;
+                wasPressed[noControler - 1] = true;
+            }
+            
+        }
+        else if (Input.GetAxis(dpadVertical + noControler) == 0 && wasPressed[noControler - 1])
+        {
+            wasPressed[noControler - 1] = false;
+        }
+    }
+}

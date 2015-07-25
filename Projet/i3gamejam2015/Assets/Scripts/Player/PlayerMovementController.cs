bf2b999bf69e35ba363bceef04aa8dded00ace68
Florian B <f_bron@hotmail.com>
2015-07-25 17:42:13 +0200

    Better pass-through platforms, additional API for Joel (reset forces)

--- Projet/i3gamejam2015/Assets/Scripts/Player/PlayerMovementController.cs
+++ Projet/i3gamejam2015/Assets/Scripts/Player/PlayerMovementController.cs
@@ -9,7 +9,9 @@ public class PlayerMovementController : MonoBehaviour {
 	public float breakForce;
 	public float initialJumpForce;
 	public float extensionJumpForce;
+	public float wallJumpLateralForce;
 	public Transform[] groundChecks;			// A position marking where to check if the player is grounded.
+	public Transform raycastBase, wallJumpCheck;
 	public bool startsFacingRight = true;
 
 	public AudioClip jumpClip;
@@ -18,7 +20,7 @@ public class PlayerMovementController : MonoBehaviour {
 	private bool grounded = false;			// Whether or not the player is grounded.
 
 	// Executed at next iteration
-	private bool wantJump = false, wantJumpExtension = false;
+	private bool wantJump = false, wantWallJump = false, wantJumpExtension = false;
 	private int playerId;
 	private float allowJumpTime = 0;
 	private List<Vector2> pendingForcesToApply = new List<Vector2>();
@@ -26,12 +28,14 @@ public class PlayerMovementController : MonoBehaviour {
 
 	private Rigidbody2D body;
 	private InputManager inputManager;
+	private Collider2D bodyCollider;
 
 	void Awake() {
 		// Setting up references.
 		body = GetComponent<Rigidbody2D>();
 		inputManager = FindObjectOfType<InputManager>();
 		playerId = GetComponent<PlayerStateController>().GetPlayerId();
+		bodyCollider = transform.Find("bodyCollider").GetComponent<Collider2D>();
 		setFacingRight(startsFacingRight);
 	}
 
@@ -40,7 +44,7 @@ public class PlayerMovementController : MonoBehaviour {
 		bool wasGrounded = grounded;
 		grounded = false;
 		for (int i = 0; i < groundChecks.Length; i++)
-			grounded = grounded || Physics2D.Linecast(transform.position, groundChecks[i].position, 1 << LayerMask.NameToLayer("Ground"));
+			grounded = grounded || Physics2D.Linecast(raycastBase.position, groundChecks[i].position, 1 << LayerMask.NameToLayer("Ground"));
 		// Allow jumping even after we left the ground for a short period
 		if (grounded)
 			allowJumpTime = 0.1f;
@@ -48,8 +52,14 @@ public class PlayerMovementController : MonoBehaviour {
 			allowJumpTime = Mathf.Max(0, allowJumpTime - Time.deltaTime);
 
 		// If the jump button is pressed and the player is grounded then the player should jump.
-		if (inputManager.WasPressed(playerId, InputManager.A) && allowJumpTime > 0)
-			wantJump = true;
+		if (inputManager.WasPressed(playerId, InputManager.A)) {
+			// Standard way of jumping (allowed to jump)
+			if (allowJumpTime > 0)
+				wantJump = true;
+			// Or by going down while on a wall
+			else if (body.velocity.y < 0 && Physics2D.Linecast(raycastBase.position, wallJumpCheck.position, 1 << LayerMask.NameToLayer("Ground")))
+				wantWallJump = true;
+		}
 		if (inputManager.IsHeld(playerId, InputManager.A) && wantJumpExtension)
 			wantJumpExtension &= body.velocity.y > 0;		// not able to extend jump anymore when grounded
 		else
@@ -77,7 +87,7 @@ public class PlayerMovementController : MonoBehaviour {
 		body.velocity = velocity;
 
 		// If the player should jump...
-		if (wantJump) {
+		if (wantJump || wantWallJump) {
 			AudioSource.PlayClipAtPoint(jumpClip, body.position);
 
 			// Ensure that the current vy doesn't take in account
@@ -87,8 +97,12 @@ public class PlayerMovementController : MonoBehaviour {
 			// Add a vertical force to the player.
 			body.AddForce(new Vector2(0.0f, initialJumpForce));
 
+			// Also move the player away from the wall
+			if (wantWallJump)
+				body.AddForce(new Vector2(isFacingRight() ? -wallJumpLateralForce : wallJumpLateralForce, 0));
+
 			// Make sure the player can't jump again until the jump conditions from Update are satisfied.
-			wantJump = false;
+			wantJump = wantWallJump = false;
 			wantJumpExtension = true;
 		}
 		else if (wantJumpExtension) {
@@ -106,6 +120,11 @@ public class PlayerMovementController : MonoBehaviour {
 
 	public bool isFacingRight() {
 		return transform.localScale.x > 0;
+	}
+
+	// Freezes the player until an applyForce is called
+	public void resetForces() {
+		body.velocity = new Vector2(0, 0);
 	}
 
 	public void setFacingRight(bool facingRight) {

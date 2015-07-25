4e38ee8a5cfca5a751d07f2bd7a1b075790f531d
davidroulin <freelance@davidroulin.ch>
2015-07-25 13:19:29 +0200

    Pixel perfect normal map experiments

---
+++ Projet/davidroulin/Assets/Scripts/MoveLight.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveLight : MonoBehaviour {
+
+	float minX = -4.0f;
+	float maxX = 5.0f;
+	Vector3 iterate = new Vector3( -10.0f, 0, 0 );
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate ( iterate * Time.deltaTime );
+		if (transform.position.x > maxX || transform.position.x < minX) {
+			iterate = - iterate;
+		}
+	}
+}

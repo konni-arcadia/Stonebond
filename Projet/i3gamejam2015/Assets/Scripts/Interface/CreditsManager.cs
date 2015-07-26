afc267e8df1a8144ccac3cd9cffbc4de01a7928f
Karim <kcheurfi@outlook.com>
2015-07-26 16:05:03 +0200

    Ajout des crédits

---
+++ Projet/i3gamejam2015/Assets/Scripts/Interface/CreditsManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 1; i < 5; i++ )
+            CheckControlerStartMenu(i);
+	}
+
+    void CheckControlerStartMenu(int noControler)
+    {
+
+        if (Input.GetButtonDown(InputManager.B + " P" + noControler))
+        {
+            Application.LoadLevelAdditiveAsync("SelectOption");
+
+            Destroy(gameObject);
+
+        }
+    }
+}

1532eeaac35eb30406f08f65edc2c61ba3104936
Daniel Néphi Muller <daniel@sunnysidegames.ch>
2015-11-28 18:37:54 +0100

    Animation FX for bond link and particles

---
+++ Projet/i3gamejam2015/Assets/Scripts/FX/BondLinkFX.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BondLinkFX : MonoBehaviour 
+{
+	public List<ParticleSystem> magicalParticles;
+	public List<ParticleSystem> lineParticles;
+	public Animator contactAnimator;
+	
+	protected int playerId;
+	
+	public void SetPlayer(int playerId)
+	{
+		this.playerId = playerId;
+	}
+
+	
+	ParticleSystem GetLineParticles()
+	{
+		return lineParticles[playerId];
+	}
+	
+	ParticleSystem GetMagicalParticles()
+	{
+		return magicalParticles[playerId];
+	}
+	
+	public void Create()
+	{
+		GetLineParticles().gameObject.SetActive(true);
+		GetMagicalParticles().gameObject.SetActive(true);
+		contactAnimator.SetTrigger("Create");
+	}
+	
+	public void Break()
+	{
+		GetLineParticles().gameObject.SetActive(false);
+		GetMagicalParticles().gameObject.SetActive(false);
+		contactAnimator.SetTrigger("Break");
+	}
+}

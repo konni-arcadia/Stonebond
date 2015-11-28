a05fa07c77fd87c8a8f6a725bf1ba0df9f4bb6fc
davidroulin <freelance@davidroulin.ch>
2015-11-28 20:33:19 +0100

    forward particles work

--- Projet/i3gamejam2015/Assets/Scripts/BondLink.cs
+++ Projet/i3gamejam2015/Assets/Scripts/BondLink.cs
@@ -3,8 +3,14 @@ using System.Collections;
 
 public class BondLink : MonoBehaviour {
 
-	public GameObject emitterA;
-	public GameObject emitterB;
+	public GameObject playerA;
+	public GameObject playerB;
+
+	public PlayerStateController playerAStateController;
+	public PlayerStateController playerBStateController;
+	
+	protected BondLinkFX fxA;
+	protected BondLinkFX fxB;
 
 	public GameObject emitterAsource;
 	public GameObject emitterBsource;
@@ -12,8 +18,20 @@ public class BondLink : MonoBehaviour {
 	public GameObject emitterAlinkMiddle;
 	public GameObject emitterBlinkMiddle;
 
-	public GameObject magicalParticlesAcontainer;
-	public GameObject magicalParticlesBcontainer;
+	protected GameObject magicalParticlesAcontainer;
+	protected GameObject magicalParticlesBcontainer;
+	
+	protected ParticleSystem magicalParticlesAsystem;
+	protected ParticleSystem magicalParticlesBsystem;
+
+	public GameObject forwardLineColliderA;
+	public GameObject forwardLineColliderB;
+
+//	public GameObject backwardParticlesAcontainer;
+//	public GameObject backwardParticlesBcontainer;
+	
+	public GameObject backwardParticlesAcollider;
+	public GameObject backwardParticlesBcollider;
 	
 	//protected GameObject linkMiddle;
 	protected Renderer rend;
@@ -23,6 +41,12 @@ public class BondLink : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+		fxA = emitterAsource.GetComponent<BondLinkFX> ();
+		fxB = emitterBsource.GetComponent<BondLinkFX> ();
+
+		playerAStateController = playerA.GetComponent<PlayerStateController> ();
+		playerBStateController = playerB.GetComponent<PlayerStateController> ();
 
 		//linkMiddle = transform.Find ("testAnimLink01").gameObject;
 		//rend = linkMiddle.GetComponent<Renderer> ();
@@ -37,11 +61,19 @@ public class BondLink : MonoBehaviour {
 
 	public void OnBond() {
 //		ParticleSystem particles
+
+//		fxA.GetLineParticles ();
+//		fxB.GetLineParticles ();
+
+		//magicalParticlesAcontainer
+		//magicalParticlesBcontainer
+//		magicalParticlesAsystem
+//		magicalParticlesBsystem
 	}
 	
 	Vector3 castLine() {
-		Vector3 a = emitterA.transform.position;
-		Vector3 b = emitterB.transform.position;
+		Vector3 a = playerA.transform.position;
+		Vector3 b = playerB.transform.position;
 		a.z = originalZ;
 		b.z = originalZ;
 		return b - a;
@@ -51,25 +83,47 @@ public class BondLink : MonoBehaviour {
 
 		// first we cast the line between the two gargoyles (the simple math is deferred to a sub-function, for readability)
 		Vector3 linkLine = castLine ();
-		Vector3 linkLineMiddlePoint = emitterA.transform.position + linkLine * 0.5f;
-		Vector3 linkLineQuarterLengthPoint = emitterA.transform.position + linkLine * 0.25f;
+		Vector3 linkLineMiddlePoint = playerA.transform.position + linkLine * 0.5f;
+		Vector3 linkLineQuarterLengthPoint = playerA.transform.position + linkLine * 0.25f;
 
 //		float angle = Mathf.Atan2(linkLine.y, linkLine.x) * Mathf.Rad2Deg;
 //		linkMiddle.transform.rotation = Quaternion.AngleAxis (angle, Vector3.forward);
 //
-		emitterAsource.transform.position = emitterA.transform.position;
+
+		//
+		// Met à jour la géométrie du lien
+		//
+
+		// Bouge les deux points source et linkmiddle
+		emitterAsource.transform.position = playerA.transform.position;
 		emitterAlinkMiddle.transform.position = linkLineMiddlePoint;
 
-
+		// Tourne les deux points source et linkmiddle pour qu'ils se regardent
 		float angle = Mathf.Atan2(linkLine.y, linkLine.x) * Mathf.Rad2Deg;
 		angle -= 90;
 		emitterAsource.transform.rotation = Quaternion.AngleAxis (angle, Vector3.forward);
 		emitterAlinkMiddle.transform.rotation = Quaternion.AngleAxis (angle, Vector3.forward);
 
+		//
+		// Met à jour les particules
+		//
 
-		magicalParticlesAcontainer .transform.position = linkLineQuarterLengthPoint;
+		// forward particles
+		forwardLineColliderA.transform.position = linkLineMiddlePoint;
+
+		// magic particles
+		magicalParticlesAcontainer.transform.position = linkLineQuarterLengthPoint;
+		// magicalParticlesAsystem = ...
+		// Faire en sorte que la shape box Y soit égale à la longueur de linkLineMiddlePoint
+
+		// backward particles
+//backwardParticlesAcontainer.transform.localPosition = Vector3.zero;
+		backwardParticlesAcollider.transform.localPosition = Vector3.zero;
+
+
 
 //		emitterBlinkMiddle.transform.position = linkLineMiddlePoint;
+
 
 
 		//emitterAsource.transform.LookAt (emitterAlinkMiddle.transform, - Vector3.forward);

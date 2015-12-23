bf68f5872a84bc573e2f31d25842dda300408dac
Philippe Güdemann <philippe@gudemann.ch>
2015-12-23 21:53:09 +0100

    Specialised class for audio menu manager

---
+++ Projet/i3gamejam2015/Assets/Scripts/Audio/MenuAudioManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class MenuAudioManager : BaseAudioManager {
+
+	public AudioMixerSnapshot MainMenuSnapshot;
+	public AudioMixerSnapshot SelectSceneSnapshot;
+	public AudioMixerSnapshot SelectCharacterSnapshot;
+	public AudioMixerSnapshot DefaultSnapshot;
+
+	// Use this for initialization
+	void Start () 
+	{
+		DefaultSnapshot.TransitionTo(5f);
+	}
+}

8b6220872cf97b7772e913446306fae62a2177e9
Philippe Güdemann <philippe@gudemann.ch>
2015-12-25 22:37:02 +0100

    Added Music Audio Manager

---
+++ Projet/i3gamejam2015/Assets/Scripts/Audio/MusicAudioManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+[PrefabAttribute("Resources/Prefabs/Audio")]
+public class MusicAudioManager : BaseAudioManager {
+
+	public AudioMixerSnapshot SpireHighSnapshot;
+	public AudioMixerSnapshot PipesOfAwakeningSnapshot;
+	public AudioMixerSnapshot CloisterOfTheSilenceSnapshot;
+	public AudioMixerSnapshot RosetteOfTheWingeSnapshot;
+	public AudioMixerSnapshot DefaultSnapshot;
+
+	void Awake() {
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	void Start () 
+	{
+		DefaultSnapshot.TransitionTo(0f);
+	}
+
+	public void SetPipesOfAwakeningSnapshot()
+	{
+		PipesOfAwakeningSnapshot.TransitionTo(0f);
+	}
+
+	public void SetSpireHighSnapshot()
+	{
+		SpireHighSnapshot.TransitionTo(0f);
+	}
+}

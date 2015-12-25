14efe4249235b8dd43d8d261cc3fcb3114ec1192
Philippe Güdemann <philippe@gudemann.ch>
2015-12-25 21:01:12 +0100

    Added bpm and transition in and out properties in base

--- Projet/i3gamejam2015/Assets/Scripts/Audio/BaseAudioManager.cs
+++ Projet/i3gamejam2015/Assets/Scripts/Audio/BaseAudioManager.cs
@@ -5,4 +5,16 @@ using UnityEngine.Audio;
 public class BaseAudioManager : MonoBehaviour {
 
 	public AudioMixerGroup mixerGroup;
+	protected float BPM = 110;
+	protected float quarterNote;
+	protected float transitionIn;
+	protected float transitionOut;
+
+
+	void start()
+	{
+		quarterNote = 60f / BPM;
+		transitionIn = quarterNote / 8f;
+		transitionOut = quarterNote * 8f;
+	}
 }

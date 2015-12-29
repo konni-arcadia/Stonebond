95a16a77613f03eeea28487a139e46c2d601ce05
Philippe Güdemann <philippe@gudemann.ch>
2015-12-29 20:45:48 +0100

    Added last sf

--- Projet/i3gamejam2015/Assets/Scripts/Audio/SfxAudioManager.cs
+++ Projet/i3gamejam2015/Assets/Scripts/Audio/SfxAudioManager.cs
@@ -13,6 +13,14 @@ public class SfxAudioManager : BaseAudioManager {
 	public AudioClip Cancel;
 	public AudioClip BoundStart;
 	public AudioClip BoundStop;
+	public AudioClip Jump;
+	public AudioClip WallJump;
+	public AudioClip AttackA;
+	public AudioClip AttackB;
+	public AudioClip Knockback;
+	public AudioClip Land;
+	public AudioClip Death;
+	public AudioClip Rebirth;
 
 	private AudioSource _source;
 
@@ -46,7 +54,6 @@ public class SfxAudioManager : BaseAudioManager {
 	public void PlayCursor()
 	{
 		GetAudioSource().PlayOneShot(Cursor);
-		SetDefaultSnapshot();
 	}
 
 	public void PlayValidate()
@@ -57,5 +64,34 @@ public class SfxAudioManager : BaseAudioManager {
 	public void PlayCancel()
 	{
 		GetAudioSource().PlayOneShot(Cancel);
+	}
+
+	public void PlayAttack()
+	{
+
+		float r = Random.Range(0, 2);
+		if (r > 0.5f)
+		{
+			GetAudioSource().PlayOneShot(AttackA);
+		}
+		else
+		{
+			GetAudioSource().PlayOneShot(AttackB);
+		}
+	}
+
+	public void PlayDeath()
+	{
+		GetAudioSource().PlayOneShot(Death);
+	}
+
+	public void PlayReBirth()
+	{
+		GetAudioSource().PlayOneShot(Rebirth);
+	}
+
+	public void PlayKnockBack()
+	{
+		GetAudioSource().PlayOneShot(Rebirth);
 	}
 }

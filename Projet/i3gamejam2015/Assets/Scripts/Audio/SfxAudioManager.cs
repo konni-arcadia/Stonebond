2c15e136ffb3c3c47f4db02d178ff08c2ff72572
Philippe Güdemann <philippe@gudemann.ch>
2015-12-29 08:54:51 +0100

    Added sf audio manager prefab and scripts

---
+++ Projet/i3gamejam2015/Assets/Scripts/Audio/SfxAudioManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+[PrefabAttribute("Prefabs/Audio/SFX/SfxAudioManager")]
+public class SfxAudioManager : BaseAudioManager {
+
+	public AudioMixerSnapshot BoundSnapshot;
+	public AudioMixerSnapshot DefaultSnapshot;
+
+	public AudioClip VictoryJingle;
+
+
+	public void SetDefaultSnapshot()
+	{
+		DefaultSnapshot.TransitionTo(0f);
+	}
+
+	public void SetBoundSnapshot()
+	{
+		BoundSnapshot.TransitionTo(0f);
+	}
+
+
+	public void VictoryJinglePlay()
+	{
+		var source = GetAudioSource(this.gameObject.name);
+		source.PlayOneShot(VictoryJingle);
+	}
+}

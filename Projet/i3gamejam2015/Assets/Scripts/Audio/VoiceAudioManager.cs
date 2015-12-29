8462ac66979f9a793f30d7a89d96b5ccc91f58d3
Philippe Güdemann <philippe@gudemann.ch>
2015-12-29 08:53:11 +0100

    Added VoiceAudio Manager Prefab and script

---
+++ Projet/i3gamejam2015/Assets/Scripts/Audio/VoiceAudioManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+[PrefabAttribute("Prefabs/Audio/Voice/VoiceAudioManager")]
+public class VoiceAudioManager : BaseAudioManager {
+
+	public AudioClip VOICETitle;
+	public AudioClip VOICECharacterSelect;
+	public AudioClip VOICEGetReady;
+	public AudioClip VOICEFight;
+	public AudioClip VOICEGameover;
+
+
+	public void TitlePlay()
+	{
+		var source = GetAudioSource(this.gameObject.name);
+		source.PlayOneShot(VOICETitle);
+	}
+
+	public void SelectCharacterPlay()
+	{
+		var source = GetAudioSource(this.gameObject.name);
+		source.PlayOneShot(VOICECharacterSelect);
+	}
+}

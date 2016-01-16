b7411ed9f3c4a97ddc5bc6a5752b4f8ea4b25ef8
Elias Farhan <elias.farhan@gmail.com>
2016-01-16 09:39:08 +0100

    Clean a bit the InputManager and added some log messages in case of missing InControl Input Manager

--- Projet/i3gamejam2015/Assets/Scripts/InputManager.cs
+++ Projet/i3gamejam2015/Assets/Scripts/InputManager.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 
-public class InputManager : MonoBehaviour {
+public class InputManager : Singleton<InputManager> {
 	//private const float AxisDeadZone = 0.6f;
 	public const float AxisDeadZone = 0.1f;
 	// Buttons
@@ -22,87 +22,105 @@ public class InputManager : MonoBehaviour {
 	}
 	
 	void Update () {
+		
 	}
+
 	public float AxisValue(int playerId, string axisName)
 	{
 		int controllerId = GameState.Instance.Player(playerId).ControllerId;
 		return AxisValueCtrl (controllerId, axisName);
 	}
-	public float AxisValueCtrl(int controllerId, string axisName) {
-		
-		/*		float value = Input.GetAxis(axisName + playerId);
-		if (value > 0) return Mathf.Max(0, value - AxisDeadZone) / (1 - AxisDeadZone);
-		else return Mathf.Min(0, value + AxisDeadZone) / (1 - AxisDeadZone);*/
-		float axisValue = 0.0f;
-		var inputDevice = (InControl.InputManager.Devices.Count > controllerId-1) ? InControl.InputManager.Devices[controllerId-1] : null;
-		if (inputDevice != null) {
-			switch (axisName) {
-			case InputManager.Horizontal:
-				axisValue += inputDevice.LeftStickX;
-				break;
-			case InputManager.Vertical:
-				axisValue -= inputDevice.LeftStickY;
-				break;
-			}
-		}
-		axisValue += Input.GetAxis (axisName + controllerId);
-		
-		return axisValue;
-	}
-	
-	public bool IsHeld(int playerId, string keyName) {
-		int controllerId = GameState.Instance.Player(playerId).ControllerId;
-		bool axisValue = false;
-		var inputDevice = (InControl.InputManager.Devices.Count > controllerId-1) ? InControl.InputManager.Devices[controllerId-1] : null;
-		if (inputDevice != null) {
-			switch (keyName) {
-			case InputManager.A: 
-				axisValue = axisValue || inputDevice.Action1.IsPressed;
-				break;
-			case InputManager.B:
-				axisValue = axisValue || inputDevice.Action2.IsPressed;
-				break;
-			case InputManager.BUTTON_ATTACK:
-				axisValue = axisValue || inputDevice.Action3.IsPressed;
-				break;
-			case InputManager.BUTTON_CHARGE:
-				axisValue = axisValue || inputDevice.Action4.IsPressed;
-				break;
-			}
-		}
-		axisValue = axisValue || Input.GetButton (keyName + " P" + controllerId);
-		return axisValue;
-	}
+
 	public bool WasPressed(int playerId, string keyName)
 	{
 		int controllerId = GameState.Instance.Player(playerId).ControllerId;
 		return WasPressedCtrl (controllerId, keyName);
 	}
+	private float CapAxis(float axisValue)
+	{
+		if (Mathf.Abs (axisValue) > 1.0f)
+			axisValue = axisValue / Mathf.Abs (axisValue);
+		return axisValue;
+	}
+	public float AxisValueCtrl(int controllerId, string axisName) {
+		float axisValue = 0.0f;
+		if (InControl.InputManager.IsSetup) {
+			var inputDevice = (InControl.InputManager.Devices.Count > controllerId - 1) ? InControl.InputManager.Devices [controllerId - 1] : null;
+			if (inputDevice != null) {
+				switch (axisName) {
+				case InputManager.Horizontal:
+					axisValue += inputDevice.LeftStickX;
+					break;
+				case InputManager.Vertical:
+					axisValue -= inputDevice.LeftStickY;
+					break;
+				}
+			}
+		} else {
+			Debug.Log ("InControl Input Manager missing, contact Elias.");
+		}
+		axisValue += Input.GetAxis (axisName + controllerId);
+		
+		return CapAxis(axisValue);
+	}
+	
+	public bool IsHeld(int playerId, string keyName) {
+		int controllerId = GameState.Instance.Player(playerId).ControllerId;
+		bool buttonValue = false;
+		if (InControl.InputManager.IsSetup) {
+			var inputDevice = (InControl.InputManager.Devices.Count > controllerId - 1) ? InControl.InputManager.Devices [controllerId - 1] : null;
+			if (inputDevice != null) {
+				switch (keyName) {
+				case InputManager.A: 
+					buttonValue = buttonValue || inputDevice.Action1.IsPressed;
+					break;
+				case InputManager.B:
+					buttonValue = buttonValue || inputDevice.Action2.IsPressed;
+					break;
+				case InputManager.BUTTON_ATTACK:
+					buttonValue = buttonValue || inputDevice.Action3.IsPressed;
+					break;
+				case InputManager.BUTTON_CHARGE:
+					buttonValue = buttonValue || inputDevice.Action4.IsPressed;
+					break;
+				}
+			}
+		} else {
+			Debug.Log ("InControl Input Manager missing, contact Elias.");
+		}
+		buttonValue = buttonValue || Input.GetButton (keyName + " P" + controllerId);
+		return buttonValue;
+	}
+
 	public bool WasPressedCtrl(int controllerId, string keyName) {
 		
-		bool axisValue = false;
-		var inputDevice = (InControl.InputManager.Devices.Count > controllerId-1) ? InControl.InputManager.Devices[controllerId-1] : null;
-		if (inputDevice != null) {
-			switch (keyName) {
-			case InputManager.A: 
-				axisValue = axisValue || inputDevice.Action1.WasPressed;
-				break;
-			case InputManager.B:
-				axisValue = axisValue || inputDevice.Action2.WasPressed;
-				break;
-			case InputManager.BUTTON_ATTACK:
-				axisValue = axisValue || inputDevice.Action3.WasPressed;
-				break;
-            case InputManager.BUTTON_CHARGE:
-                axisValue = axisValue || inputDevice.Action4.WasPressed;
-                break;
-			case InputManager.START:
-				axisValue = axisValue || inputDevice.MenuWasPressed;
-				break;
+		bool buttonValue = false;
+		if (InControl.InputManager.IsSetup) {
+			var inputDevice = (InControl.InputManager.Devices.Count > controllerId - 1) ? InControl.InputManager.Devices [controllerId - 1] : null;
+			if (inputDevice != null) {
+				switch (keyName) {
+				case InputManager.A: 
+					buttonValue = buttonValue || inputDevice.Action1.WasPressed;
+					break;
+				case InputManager.B:
+					buttonValue = buttonValue || inputDevice.Action2.WasPressed;
+					break;
+				case InputManager.BUTTON_ATTACK:
+					buttonValue = buttonValue || inputDevice.Action3.WasPressed;
+					break;
+				case InputManager.BUTTON_CHARGE:
+					buttonValue = buttonValue || inputDevice.Action4.WasPressed;
+					break;
+				case InputManager.START:
+					buttonValue = buttonValue || inputDevice.MenuWasPressed;
+					break;
 				
+				}
 			}
+		} else {
+			Debug.Log ("InControl Input Manager missing, contact Elias.");
 		}
-		axisValue = axisValue || Input.GetButtonDown (keyName + " P" + controllerId);
-		return axisValue;
+		buttonValue = buttonValue || Input.GetButtonDown (keyName + " P" + controllerId);
+		return buttonValue;
 	}
 }

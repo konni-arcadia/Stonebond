abea10a2ccf2517c47d39e2106cc8947d662c979
Karim <kcheurfi@outlook.com>
2016-01-16 12:39:10 +0100

    Début squelet crédits déroulants

--- Projet/i3gamejam2015/Assets/Scripts/Interface/CreditsManager.cs
+++ Projet/i3gamejam2015/Assets/Scripts/Interface/CreditsManager.cs
@@ -4,6 +4,12 @@ using System.Collections;
 public class CreditsManager : MonoBehaviour {
 
 	private InputManager inputManager;
+
+    public RectTransform canvas;
+    public RectTransform viewport;
+
+    public int ScrollSpeed = 3;
+
 	// Use this for initialization
 	void Start () {
 		inputManager = GetComponent<InputManager> ();
@@ -13,6 +19,15 @@ public class CreditsManager : MonoBehaviour {
 	void Update () {
         for (int i = 1; i < 5; i++ )
             CheckControlerStartMenu(i);
+
+        if (canvas.sizeDelta.y < canvas.anchoredPosition.y)
+        {
+            canvas.anchoredPosition = new Vector3(0, -viewport.sizeDelta.y, 0);
+        }
+        else
+        {
+            canvas.anchoredPosition = new Vector3(0, canvas.anchoredPosition.y + ScrollSpeed, 0);
+        }
 	}
 
     void CheckControlerStartMenu(int noControler)

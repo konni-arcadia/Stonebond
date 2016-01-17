f9b5d105e00659a5f3350cfe8cbb9ddeef70f49e
Florian B <f_bron@hotmail.com>
2016-01-17 14:50:56 +0100

    Stops the flash when going back to the menu (issue #34)

--- Projet/i3gamejam2015/Assets/Scripts/Interface/MenuInitialManager.cs
+++ Projet/i3gamejam2015/Assets/Scripts/Interface/MenuInitialManager.cs
@@ -5,19 +5,23 @@ using UnityEngine.UI;
 
 public class MenuInitialManager : MonoBehaviour {
 
+	// The background will be shown only if we're not coming from another menu
+	public GameObject Background;
 	public GameObject FadePanel;
 
 	// Use this for initialization
 	void Start () {
         if (PlayerPrefs.HasKey("ComeFromLVL"))
         {
+			Background.SetActive(false);
             PlayerPrefs.DeleteKey("ComeFromLVL");
 			SceneManager.LoadSceneAsync("SelectLvl", LoadSceneMode.Additive);
         }
         else
         {
-            if (GameState.Instance != null)
+			if (GameState.Instance != null)
                 GameState.Instance.ClearScores();
+			Background.SetActive(true);
 			FadePanel.GetComponent<Image>().color = Color.black;
 			StartCoroutine(FadeLater());
         }

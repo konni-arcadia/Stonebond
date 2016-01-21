941e96618188c4490446912ff773dd85fd09c7ca
Philippe Güdemann <philippe@gudemann.ch>
2016-01-21 21:56:21 +0100

    Issue #57

--- Projet/i3gamejam2015/Assets/Scripts/Interface/CreditsManager.cs
+++ Projet/i3gamejam2015/Assets/Scripts/Interface/CreditsManager.cs
@@ -14,6 +14,7 @@ public class CreditsManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		inputManager = GetComponent<InputManager> ();
+		AudioSingleton<MenuAudioManager>.Instance.SetMainMenuSnapshot();
 	}
 	
 	// Update is called once per frame

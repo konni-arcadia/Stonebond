70fa46788fc846953c11bb213e705ae2f7ac369a
davidroulin <freelance@davidroulin.ch>
2016-01-23 12:35:03 +0100

    Added acceleration parameter to scripted particle animations

--- Projet/i3gamejam2015/Assets/Scripts/FX/UnpausableParticleSystem.cs
+++ Projet/i3gamejam2015/Assets/Scripts/FX/UnpausableParticleSystem.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class UnpausableParticleSystem : MonoBehaviour {
 
+	// This is needed to speed up particle simulation in the case of forward particles
+	// during chest blast. Because we move the end collider very quickly and particles
+	// don't have time to spawn quick enough to draw a beam.
+	// So for those particles we make that happen by giving a big acceleration.
+	public float acceleration = 1.0f;
+
     private ParticleSystem particles;
 
     private bool wasPaused = false;
@@ -16,7 +22,7 @@ public class UnpausableParticleSystem : MonoBehaviour {
     {
         if (TimeManager.isPaused)
         {
-            particles.Simulate(TimeManager.realDeltaTime, true, false);
+			particles.Simulate(TimeManager.realDeltaTime * acceleration, true, false);
             wasPaused = true;
         }
         else if(wasPaused)

b82b5781f730007a3d3cd17e871ce9e9d3394040
Joel Lauener <joellauener@hotmail.com>
2016-06-29 14:47:22 +0200

    tuned gargoyles colors and chroma curves

--- Projet/i3gamejam2015/Assets/Scripts/Player/PlayerFXManager.cs
+++ Projet/i3gamejam2015/Assets/Scripts/Player/PlayerFXManager.cs
@@ -6,27 +6,21 @@ public class PlayerFXManager : MonoBehaviour
     // CHROMA
     //
 
-    //public float bodyColorDarkPct = 0.1f;
-    //public float bodyColorBrightPct = 0.1f;
-    //public float chromaColorDarkPct = 1.0f;
-    //public float chromaColorBrightPct = 0.9f;
-
-    public float normalChromaLight = 0.6f;
-
-    //
-    // ATTACK
-    //
+    public float chromaLightNormal = 0.6f;
+    public float chromaLightMin = 0.0f;
+    public float chromaLightMax = 1.0f;    
 
     public AnimationCurve attackForwardChromaCurve;
     public AnimationCurve attackUpChromaCurve;
     public AnimationCurve attackDownChromaCurve;
-    public AnimationCurve attackSpecialChromaCurve;
     public AnimationCurve attackCooldownChromaCurve;
+    public AnimationCurve respawnChromaCurve;
+    public AnimationCurve killedChromaCurve;    
 
     //
     // OVERLAY
     //
- 
+
     public string overlaySortingLayerName = "HUD";
     public int overlaySortingOrder = 100;
 
@@ -39,23 +33,13 @@ public class PlayerFXManager : MonoBehaviour
     private SpriteRenderer bodyRenderer;
     private string originalSortingLayerName;
     private int originalSortingOrder;
+    private GameState.PlayerInfo playerInfo;
 
     //
     // GLOBAL STATE
     //
 
-  //  private Color bodyColorNormal;
-   // private Color bodyColorMin;
-    //private Color bodyColorMax;
-  //  private Color chromaColorNormal;
-   // private Color chromaColorMin;
-  //  private Color chromaColorMax;
-
-    // TODO use a state machine
     private bool overlay = false;
-    private bool charge = false;
-
-    private GameState.PlayerInfo playerInfo;
 
     
     //
@@ -75,9 +59,6 @@ public class PlayerFXManager : MonoBehaviour
         statusProvider.OnCollisionAction += HandleOnCollisionAction;
         statusProvider.OnAttackStartAction += HandleOnAttackStartAction;
         statusProvider.OnAttackStopAction += HandleOnAttackStopAction;
-        statusProvider.OnChargeStartAction += HandleOnChargeStartAction;
-        statusProvider.OnChargeStopAction += HandleOnChargeStopAction;
-        statusProvider.OnChargeUpdateAction += HandleOnChargeUpdateAction;
 
         originalSortingLayerName = bodyRenderer.sortingLayerName;
         originalSortingOrder = bodyRenderer.sortingOrder;
@@ -86,117 +67,65 @@ public class PlayerFXManager : MonoBehaviour
     void Start()
     {
         playerInfo = GameState.Instance.Player(stateController.GetPlayerId());
-
-        //bodyColorNormal = playerInfo.BodyColor;
-        //bodyColorMin = Color.Lerp(bodyColorNormal, Color.black, bodyColorDarkPct);
-        //bodyColorMax = Color.Lerp(bodyColorNormal, Color.white, bodyColorBrightPct);
-
-        //chromaColorNormal = playerInfo.Color;
-        //chromaColorMin = Color.Lerp(chromaColorNormal, Color.black, chromaColorDarkPct);
-        //chromaColorMax = Color.Lerp(chromaColorNormal, Color.white, chromaColorBrightPct);
-
-        //   SetBodyColor(playerInfo.BodyColor);
-        // SetChromaColor(playerInfo.Color);
-        SetChromaLight(normalChromaLight);
+        SetChromaLightCurveValue(0.0f);
     }
 
     void Update()
     {
-        if (overlay || charge)
+        if (overlay)
         {
-            // don't change the sprite color while in overlay/charge mode
+            // don't change the sprite color while in overlay mode
+        }
+        else if(stateController.IsCrystaled())
+        {
+            UpdateCrystalized();            
         }
         else if (stateController.GetAttackPct() > 0.0f)
         {
             switch (stateController.GetAimDirection())
             {
                 case PlayerStateController.AimDirection.UP:
-                    SetChromaLight(attackUpChromaCurve.Evaluate(stateController.GetAttackPct()));
+                    SetChromaLightCurveValue(attackUpChromaCurve.Evaluate(stateController.GetAttackPct()));
            
                     break;
                 case PlayerStateController.AimDirection.DOWN:
-                    SetChromaLight(attackDownChromaCurve.Evaluate(stateController.GetAttackPct()));
+                    SetChromaLightCurveValue(attackDownChromaCurve.Evaluate(stateController.GetAttackPct()));
                     break;
                 case PlayerStateController.AimDirection.FORWARD:
-                    SetChromaLight(attackForwardChromaCurve.Evaluate(stateController.GetAttackPct()));
+                    SetChromaLightCurveValue(attackForwardChromaCurve.Evaluate(stateController.GetAttackPct()));
                     break;
             }
 
-        }
-        else if (stateController.GetSpecialAttackPct() > 0.0f)
+        }                       
+        else if(stateController.GetAttackCooldownPct() > 0.0f)
         {
-            SetChromaLight(attackSpecialChromaCurve.Evaluate(stateController.GetSpecialAttackPct()));
-        }
-        else if (!stateController.IsCrystaled() && stateController.GetAttackCooldownPct() > 0.0f)
-        {
-            SetChromaLight(attackCooldownChromaCurve.Evaluate(stateController.GetAttackCooldownPct()));
-        }
-        else if(stateController.IsCrystaled())
-        {
-            SetChromaLight(0.2f);
-            //SetBodyColor(bodyColorNormal);
-            //Color.Lerp(chromaColorNormal, Color.black, 0.75f);
+            SetChromaLightCurveValue(attackCooldownChromaCurve.Evaluate(stateController.GetAttackCooldownPct()));
         }
         else
         {
-            SetChromaLight(normalChromaLight);
-           // SetBodyColor(bodyColorNormal);
-            //SetChromaColor(chromaColorNormal);
+            SetChromaLightCurveValue(chromaLightNormal);
         }
     }
 
     //
-    // CHARGE
+    // CRYSTALIZED
     //
 
-    public float chargeLoadChromaMin = 0.05f;
-    public float chargeLoadChromaMax = 0.5f;
-    public float chargeReadyChromaMin = 0.55f;
-    public float chargeReadyChromaMax = 1.0f;
-    public float chargeOverflowBlinkInterval = 0.01f;
-    public AnimationCurve chargeLoadChromaCurve;
+    private float crystalizedElapsedTime = 100.0f; // small hack to prevent killedChromaCurve on first spawn
+    public float killedChromaDuration = 0.4f;
 
-    private float chargeOverflowBlinkCounter;
-    private bool chargeOverflowBlinkVisible;
-
-    private void HandleOnChargeStartAction()
+    private void UpdateCrystalized()
     {
-        charge = true;
-        chargeOverflowBlinkCounter = 0.0f;
-        chargeOverflowBlinkVisible = true;
-    }
+        crystalizedElapsedTime += Time.deltaTime;
 
-    private void HandleOnChargeStopAction(bool complete)
-    {
-        charge = false;
-    }
-
-    private void HandleOnChargeUpdateAction(PlayerStatusProvider.ChargeState state, float statePct, float forceRatio)
-    {
-        if (overlay)
+        if(crystalizedElapsedTime < killedChromaDuration)
         {
-            return;
+            SetChromaLightCurveValue(killedChromaCurve.Evaluate(crystalizedElapsedTime / killedChromaDuration));
         }
-
-        switch (state)
+        else
         {
-            case PlayerStatusProvider.ChargeState.LOAD:
-                SetChromaLight(chargeLoadChromaMin + chargeLoadChromaCurve.Evaluate(statePct) * (chargeLoadChromaMax - chargeLoadChromaMin));
-                break;
-            case PlayerStatusProvider.ChargeState.READY:
-                SetChromaLight(chargeReadyChromaMin + forceRatio * (chargeReadyChromaMax - chargeReadyChromaMin));
-                break;
-            case PlayerStatusProvider.ChargeState.FULL:
-                chargeOverflowBlinkCounter -= Time.deltaTime;
-                if (chargeOverflowBlinkCounter <= 0.0)
-                {
-                    chargeOverflowBlinkVisible = !chargeOverflowBlinkVisible;
-                    chargeOverflowBlinkCounter += chargeOverflowBlinkInterval;
-                }
-                SetChromaLight(chargeOverflowBlinkVisible ? chargeLoadChromaMax : -1.0f);
-                break;
+            SetChromaLightCurveValue(respawnChromaCurve.Evaluate(stateController.GetRespawnPct()));
         }
-
     }
 
     //
@@ -224,7 +153,7 @@ public class PlayerFXManager : MonoBehaviour
             PlayerFXManager sourceFXManager = source.GetComponent<PlayerFXManager>();
             sourceFXManager.SetOverlay(true);
             sourceFXManager.SetShadow();
-        }
+        }        
     }
 
     private void HandleOnDie(Transform source, Vector2 attackDirection)
@@ -240,6 +169,8 @@ public class PlayerFXManager : MonoBehaviour
             PlayerFXManager sourceFXManager = source.GetComponent<PlayerFXManager>();
             sourceFXManager.SetOverlay(false);
         }
+
+        crystalizedElapsedTime = 0.0f;
     }
 
     private void HandleOnCollisionAction(PlayerStatusProvider.CollisionType collisionType, Vector2 normal)
@@ -260,84 +191,45 @@ public class PlayerFXManager : MonoBehaviour
 
     private void HandleOnAttackStartAction(PlayerStatusProvider.AttackType attackType, Vector2 direction)
     {
-        if (attackType == PlayerStatusProvider.AttackType.SPECIAL)
-        {
-            float angle = Mathf.Sign(direction.y) * Vector2.Angle(direction.x < 0.0f ? Vector2.left : Vector2.right, direction);
-            bodyRenderer.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-            bodyRenderer.transform.parent.Translate(new Vector3(0.0f, 1.0f, 0.0f));
-        }
+        // nothing to do
     }
 
     private void HandleOnAttackStopAction(PlayerStatusProvider.AttackType attackType, bool cancelled)
     {
-        if (attackType == PlayerStatusProvider.AttackType.SPECIAL)
-        {
-            bodyRenderer.transform.rotation = Quaternion.AngleAxis(0.0f, Vector3.forward);
-            bodyRenderer.transform.parent.Translate(new Vector3(0.0f, -1.0f, 0.0f));
-        }
+        // nothing to do
     }
 
     //
     // HELPERS
     //
 
-    private void SetChromaLight(float value)
+    private void SetChromaLightCurveValue(float curveValue)
     {
+        float value;
+        if(curveValue < 0.0f)
+        {
+            value = chromaLightNormal + curveValue * (chromaLightNormal - chromaLightMin);
+        }
+        else if(curveValue > 0.0f)
+        {
+            value = chromaLightNormal + curveValue * (chromaLightMax - chromaLightNormal);
+        }
+        else
+        {
+            value = chromaLightNormal;
+        }
+
         bodyRenderer.material.SetColor("_BodyColor", playerInfo.BodyColor);
         bodyRenderer.material.SetColor("_ChromaColor", playerInfo.Color);
-        bodyRenderer.material.SetFloat("_ChromaLight", Mathf.Clamp(value, 0.0f, 1.0f));
-        //SetBodyColor(value);
-        //SetChromaColor(value);
+        bodyRenderer.material.SetFloat("_ChromaLightPct", Mathf.Clamp(value, 0.0f, 1.0f));
     }
 
     private void SetShadow()
     {
         bodyRenderer.material.SetColor("_BodyColor", Color.black);
         bodyRenderer.material.SetColor("_ChromaColor", Color.black);
-        bodyRenderer.material.SetFloat("_ChromaLight", 0.0f);
+        bodyRenderer.material.SetFloat("_ChromaLightPct", 0.0f);
     }
-
-    //private void SetChromaColor(float value)
-    //{
-    //    if (value < 0.0f)
-    //    {
-    //        SetChromaColor(Color.Lerp(chromaColorNormal, chromaColorMin, Mathf.Min(1.0f, Mathf.Abs(value))));
-    //    }
-    //    else if (value > 0.0f)
-    //    {
-    //        SetChromaColor(Color.Lerp(chromaColorNormal, chromaColorMax, Mathf.Min(1.0f, value)));
-    //    }
-    //    else
-    //    {
-    //        SetChromaColor(chromaColorNormal);
-    //    }
-    //}
-
-    //private void SetBodyColor(float value)
-    //{
-    //    if (value < 0.0f)
-    //    {
-    //        SetBodyColor(Color.Lerp(bodyColorNormal, bodyColorMin, Mathf.Min(1.0f, Mathf.Abs(value))));
-    //    }
-    //    else if (value > 0.0f)
-    //    {
-    //        SetBodyColor(Color.Lerp(bodyColorNormal, bodyColorMax, Mathf.Min(1.0f, value)));
-    //    }
-    //    else
-    //    {
-    //        SetBodyColor(chromaColorNormal);
-    //    }
-    //}
-
-    //private void SetBodyColor(Color color)
-    //{
-    //    bodyRenderer.material.SetColor("_BodyColor", color);
-    //}
-
-    //private void SetChromaColor(Color color)
-    //{
-    //    bodyRenderer.material.SetColor("_ChromaColor", color);
-    //}
 
     private void SetOverlay(bool overlay)
     {

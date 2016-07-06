177594cccab4c05daea06990ef7772fdcd9b14f5
davidroulin <freelance@davidroulin.ch>
2016-07-06 16:47:38 +0200

    Added final state to display assembled statue before disabling

--- Projet/i3gamejam2015/Assets/Scripts/Player/PlayerAnimationBreakDeath.cs
+++ Projet/i3gamejam2015/Assets/Scripts/Player/PlayerAnimationBreakDeath.cs
@@ -12,40 +12,61 @@ public class PlayerAnimationBreakDeath : MonoBehaviour {
 	public float explosionDuration = 2.0f;
 	public float lyingDownDuration = 1.0f;
 	public float reassemblingDuration = 1.0f;
+	public float fadingBackToInactiveDuration = 0.5f;
+	// TODO: add gravityScale (maybe)
+	// TODO: add the shaking before reassembling parameters (intervals if many shakes - re-code - and vibrato, strength, etc)
+	// TODO: add reassembling Easing function (maybe)
 
 	// Internal refs
 	private System.Random rand;
+	private GameObject bodyPartsContainer;
 	private Rigidbody2D[] rigidBodies;
 
 	// Internal state vars
 	private float internalClock;
 	private enum State {
-		Inactive, StartingDeath, Exploding, LyingDownBrokenInPieces, Reassembling
+		Inactive, StartingDeath, Exploding, LyingDownBrokenInPieces, Reassembling, FadingBackToInactive
 	}
 	private State currentState;
 
+	// DEBUG VARS -- begin
 	public Vector2 hitDirection;
 	public bool mustDie;
+	// DEBUG VARS -- end
 
 	void Start () {
-		currentState = State.Inactive;
+		bodyPartsContainer = transform.Find ("BodyParts").gameObject;
 		rand = new System.Random ();
-		rigidBodies = transform.gameObject.GetComponentsInChildren<Rigidbody2D> () as Rigidbody2D[];
+		rigidBodies = bodyPartsContainer.GetComponentsInChildren<Rigidbody2D> () as Rigidbody2D[];
 		for (int i = 0; i < rigidBodies.Length; i++) {
 			rigidBodies [i].isKinematic = true;
 			rigidBodies [i].gravityScale = 2.5f;
 		}
+		SetState (State.Inactive);
 	}
 
 	public void Die() {
 		if (currentState == State.Inactive) {
-			currentState = State.StartingDeath;
+			SetState (State.StartingDeath);
 		}
 	}
 
 	// stops ongoing stuff if needed
 	private void SetState(State newState) {
+
+		// set the state
 		currentState = newState;
+
+		// performs internal changes according to the new state
+		switch (currentState) {
+		case State.Inactive:
+			bodyPartsContainer.SetActive (false);
+			break;
+		default:
+			bodyPartsContainer.SetActive (true);
+			break;
+		}
+
 	}
 
 	// NB: this is not an Animation in the traditional meaning. It is based on the physics engine.
@@ -57,10 +78,10 @@ public class PlayerAnimationBreakDeath : MonoBehaviour {
 			return;
 		}
 
+		//Debug.Log ("BOUM");
+
 		internalClock = 0.0f;
-		
-		Debug.Log ("BOUM");
-		
+
 		SetState (State.Exploding);
 
 		directionOfHit.Normalize ();
@@ -85,6 +106,78 @@ public class PlayerAnimationBreakDeath : MonoBehaviour {
 
 	}
 
+	// NB: this is not an Animation in the traditional meaning. It is based on the physics engine.
+	private void StartLyingDownAnims() {
+
+		// foolproof
+		if (currentState == State.LyingDownBrokenInPieces) {
+			// an animation is already ongoing
+			return;
+		}
+
+		internalClock = 0.0f;
+		
+		SetState (State.LyingDownBrokenInPieces);
+
+//		Sequence seq = DOTween.Sequence ();
+//		for (int i = 0; i < rigidBodies.Length; i++) {
+//			seq.Insert (0, rigidBodies [i].transform.DOShakePosition (lyingDownDuration * 1.2f, new Vector3 (0.7f, 0.1f), 10, 180, false));
+//		}
+//		seq.Play ();
+//		seq.OnComplete (() => {
+//			//currentState = State.Inactive;
+//		});
+
+		for (int i = 0; i < rigidBodies.Length; i++) {
+			rigidBodies [i].AddForce (Vector2.up * UnityEngine.Random.Range(10, 30), ForceMode2D.Impulse);
+		}
+
+	}
+
+	// NB: this is not an Animation in the traditional meaning. It is based on the physics engine.
+	private void StartReassemblingAnim() {
+
+		// foolproof
+		if (currentState == State.Reassembling) {
+			// an animation is already ongoing
+			return;
+		}
+
+		internalClock = 0.0f;
+
+		SetState (State.Reassembling);
+
+//		for (int i = 0; i < rigidBodies.Length; i++) {
+//			rigidBodies [i].isKinematic = true;
+//			rigidBodies [i].transform.localPosition = Vector3.zero;
+//			rigidBodies [i].transform.localRotation = Quaternion.identity;
+//		}
+
+		Sequence seq = DOTween.Sequence ();
+		for (int i = 0; i < rigidBodies.Length; i++) {
+			rigidBodies [i].isKinematic = true;
+			seq.Insert (0, rigidBodies [i].transform.DOLocalMove (Vector3.zero, reassemblingDuration).SetEase(Ease.InCirc));
+			seq.Insert (0, rigidBodies [i].transform.DOLocalRotateQuaternion (Quaternion.identity, reassemblingDuration).SetEase(Ease.InCirc));
+		}
+		seq.Play ();
+		seq.OnComplete (StartFadingBackToInactiveAnim);
+
+	}
+
+	private void StartFadingBackToInactiveAnim() {
+
+		// foolproof
+		if (currentState == State.FadingBackToInactive) {
+			// an animation is already ongoing
+			return;
+		}
+
+		internalClock = 0.0f;
+
+		SetState(State.FadingBackToInactive);
+
+	}
+
 	void Update () {
 
 // DEBUG CODE
@@ -101,33 +194,15 @@ public class PlayerAnimationBreakDeath : MonoBehaviour {
 		}
 		
 		if (currentState == State.Exploding && internalClock > explosionDuration + lyingDownDuration/2) {
-			currentState = State.LyingDownBrokenInPieces;
-			internalClock = 0.0f;
-			//Sequence seq = DOTween.Sequence ();
-			for (int i = 0; i < rigidBodies.Length; i++) {
-				rigidBodies [i].AddForce (Vector2.up * UnityEngine.Random.Range(10, 30), ForceMode2D.Impulse);
-				//seq.Insert (0, rigidBodies [i].transform.DOShakePosition (lyingDownDuration * 1.2f, new Vector3 (0.7f, 0.1f), 10, 180, false));
-			}
-			//seq.Play ();
-			//seq.OnComplete (() => {
-			//	//currentState = State.Inactive;
-			//});
+			StartLyingDownAnims ();
 		}
 
 		if (currentState == State.LyingDownBrokenInPieces && internalClock > lyingDownDuration) {
-			currentState = State.Reassembling;
-			Sequence seq = DOTween.Sequence ();
-			for (int i = 0; i < rigidBodies.Length; i++) {
-				rigidBodies [i].isKinematic = true;
-//				rigidBodies [i].transform.localPosition = Vector3.zero;
-//				rigidBodies [i].transform.localRotation = Quaternion.identity;
-				seq.Insert (0, rigidBodies [i].transform.DOLocalMove (Vector3.zero, reassemblingDuration).SetEase(Ease.InCirc));
-				seq.Insert (0, rigidBodies [i].transform.DOLocalRotateQuaternion (Quaternion.identity, reassemblingDuration).SetEase(Ease.InCirc));
-			}
-			seq.Play ();
-			seq.OnComplete (() => {
-				currentState = State.Inactive;
-			});
+			StartReassemblingAnim ();
+		}
+
+		if (currentState == State.FadingBackToInactive && internalClock > fadingBackToInactiveDuration) {
+			SetState (State.Inactive);
 		}
 
 	}

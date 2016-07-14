7c0a7a766edebdbedb62668b534d94d9b455f5cf
Joel Lauener <joellauener@hotmail.com>
2016-07-14 14:50:19 +0200

    moving platform refactor started

--- Projet/i3gamejam2015/Assets/Scripts/Platforms/HorizontalMovingPlatform.cs
+++ Projet/i3gamejam2015/Assets/Scripts/Platforms/HorizontalMovingPlatform.cs
@@ -7,14 +7,16 @@ public class HorizontalMovingPlatform : MonoBehaviour {
     public Transform rightBound;
     public Transform platform;
 
-    Transform[] bounds = new Transform[2];
+    private Transform[] bounds = new Transform[2];
 
-    int currentBoundIndex = 0;
+    private int currentBoundIndex = 0;
 
     public float movementSpeed = 1f;
 
-    SurfaceEffector2D surfaceEffector;
-    float surfaceEffectorSpeed;
+    private Rigidbody2D platformRigidbody;
+
+   // private SurfaceEffector2D surfaceEffector;
+   // private float surfaceEffectorSpeed;
 
     public bool startToLeft = true;
 
@@ -22,29 +24,38 @@ public class HorizontalMovingPlatform : MonoBehaviour {
 	void Start () {
         bounds[0] = leftBound;
         bounds[1] = rightBound;
-        surfaceEffector = GetComponentInChildren<SurfaceEffector2D>();
+        //surfaceEffector = GetComponentInChildren<SurfaceEffector2D>();
         currentBoundIndex = startToLeft ? 0 : 1;
-        surfaceEffectorSpeed = movementSpeed * 5.5f;
-        surfaceEffector.speed = (startToLeft ? -1 : 1) * surfaceEffectorSpeed;
-        if (surfaceEffector == null)
-        { 
-            Debug.LogError("Surface effector can't be found in moving platform " + name);
+        //surfaceEffectorSpeed = movementSpeed * 5.5f;        
+        //if (surfaceEffector == null)
+        //{ 
+        //    Debug.LogError("Surface effector can't be found in moving platform " + name);
+        //}
+        //surfaceEffector.speed = (startToLeft ? -1 : 1) * surfaceEffectorSpeed;
+
+        platformRigidbody = platform.GetComponent<Rigidbody2D>();
+        if(platformRigidbody == null)
+        {
+            Debug.LogError("Rigidbody2D can't be found in moving platform " + name);
         }
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	    if(platform.position.x < bounds[0].position.x)
+        Vector2 position = platformRigidbody.position;
+
+        if(position.x < bounds[0].position.x)
         {
             currentBoundIndex = 1;
-            surfaceEffector.speed = surfaceEffectorSpeed;
+           // surfaceEffector.speed = surfaceEffectorSpeed;
         }
-        if(platform.position.x > bounds[1].position.x)
+        if(position.x > bounds[1].position.x)
         {
             currentBoundIndex = 0;
-            surfaceEffector.speed = -surfaceEffectorSpeed;
+            //surfaceEffector.speed = -surfaceEffectorSpeed;
         }
 
-        platform.position += new Vector3((currentBoundIndex == 0 ? -1 : 1) * movementSpeed * Time.deltaTime, 0f, 0f);
+        position.x += (currentBoundIndex == 0 ? -1 : 1) * movementSpeed * Time.deltaTime;
+        platformRigidbody.MovePosition(position);
 	}
 }
